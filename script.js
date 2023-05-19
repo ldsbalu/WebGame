98f810457f08bc0d5e8a618d98d99dd3d4ff298c
Balu Pilli <ldsbalu@gmail.com>
2023-05-19 12:11:06 -1000

    lesson 10 completed, until collision reusable code

--- script.js
+++ script.js
@@ -55,6 +55,12 @@ window.addEventListener('load', function (){
 
             this.collisionX += this.speedX * this.speedModifier;
             this.collisionY += this.speedY * this.speedModifier;
+            //collisions with obstacles
+            this.game.obstacles.forEach(obstacle => {
+                if (this.game.checkCollision(this,obstacle)){
+                    console.log('collision');
+                }
+            })
 
         }
 
@@ -68,9 +74,21 @@ window.addEventListener('load', function (){
             this.game = game;
             this.collisionX = Math.random() *this.game.width;
             this.collisionY = Math.random() *this.game.height;
-            this.collisionRadius = 100;
+            this.collisionRadius = 60;
+            this.image = document.getElementById('obstacles');
+            this.spriteWidth =250;
+            this.spriteHeight = 250;
+            this.width = this.spriteWidth;
+            this.height= this.spriteHeight;
+            this.spriteX = this.collisionX -this.width * 0.5;
+            this.spriteY = this.collisionY -this.height * 0.5 - 70;
+            this.frameX =Math.floor(Math.random() * 4);
+            this.frameY =Math.floor(Math.random() * 4);
+
+
         }
         draw(context){
+            context.drawImage(this.image, this.frameX * this.spriteWidth ,this.frameY * this.spriteHeight,this.spriteWidth,this.spriteHeight,this.spriteX,this.spriteY,this.width,this.height);
             context.beginPath();
             context.arc(this.collisionX,this.collisionY,this.collisionRadius,0,Math.PI*2);
             context.save();
@@ -78,6 +96,7 @@ window.addEventListener('load', function (){
             context.fill();
             context.restore();
             context.stroke();
+
 
         }
 
@@ -88,8 +107,9 @@ window.addEventListener('load', function (){
             this.canvas =canvas;
             this.width = this.canvas.width;
             this.height = this.canvas.height;
+            this.topMargin = 260;
             this.player = new Player(this);
-            this.numberOfObstacles = 5;
+            this.numberOfObstacles = 10;
             this.obstacles = [];
 
             this.mouse = {
@@ -123,6 +143,15 @@ window.addEventListener('load', function (){
             this.player.update();
             this.obstacles.forEach(obstacle =>obstacle.draw(context));
 
+
+        }
+        checkCollision(a,b){
+            const dx = a.collisionX -b.collisionX;
+            const dy = a.collisionY -b.collisionY;
+            const distance = Math.hypot(dy,dx);
+            const sumOfRadii = a.collisionRadius + b.collisionRadius
+            return(distance<sumOfRadii)
+
         }
         init(){
             //Brute force alogarithm implementation which wont allows cirlcles dont overlap
@@ -135,12 +164,14 @@ window.addEventListener('load', function (){
                     const dx = testObstacle.collisionX - obstacle.collisionX;
                     const dy = testObstacle.collisionY - obstacle.collisionY;
                     const distance = Math.hypot(dy,dx);
-                    const sumOfRadii = testObstacle.collisionRadius + obstacle.collisionRadius;
+                    const distanceBuffer =150;
+                    const sumOfRadii = testObstacle.collisionRadius + obstacle.collisionRadius + distanceBuffer;
                     if (distance<sumOfRadii){
                         overlap = true;
                     }
-                })
-                if(!overlap){
+                });
+                const margin = testObstacle.collisionRadius * 2;
+                if(!overlap && testObstacle.spriteX > 0 && testObstacle.spriteX < this.width - testObstacle.width && testObstacle.collisionY > this.topMargin + margin && testObstacle.collisionY < this.height - margin ){
                     this.obstacles.push(testObstacle);
                 }
                 attempts++;
